38fa75ff896a2c7d8d1d8b2c2d43583386be4aed
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2023-03-06 02:40:10 -0600

    Create Pair.cs

---
+++ Talysoft/DataTypes/Pair.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talysoft.DataTypes
+{
+    public struct Pair<T, U>
+    {
+        public T left;
+        public U right;
+
+        public Pair(T t, U u)
+        {
+            left = t;
+            right = u;
+        }
+
+        public override string ToString()
+        {
+            return $"({left}, {right})";
+        }
+    }
+
+    public struct Pair<T>
+    {
+        public T left;
+        public T right;
+
+        public Pair(T t, T u)
+        {
+            left = t;
+            right = u;
+        }
+
+        public override string ToString()
+        {
+            return $"({left}, {right})";
+        }
+    }
+}
